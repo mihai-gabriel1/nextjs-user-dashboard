3f50a354cb4790029f1b2594442721ac7600ffda
mihai-gabriel1 <98354641+mihai-gabriel1@users.noreply.github.com>
2023-05-18 23:20:43 +0300

    The three top cards have been styled; they are now fully responsive.

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="flex justify-between px-4 pt-4">
+      <h2>Dashboard</h2>
+      <h2>Welcome Back, Gabriel</h2>
+    </div>
+  );
 };
 
 export default Header;
